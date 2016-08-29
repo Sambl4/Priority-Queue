a311593203159e08b7d8f2beb9833ff58a3eb92e
Dima Samuilik <sambl4@tut.by>
2016-08-29 13:08:46 +0300

    correct max-heap & node

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,15 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+			this.root = null;
+			this.parentNodes = [];
+
 	}
 
 	push(data, priority) {
-		
+		var node = new Node (data, priority);
+		this.insertNode (node);
+		this.shiftNodeUp (node);
 	}
 
 	pop() {
@@ -14,7 +18,9 @@ class MaxHeap {
 	}
 
 	detachRoot() {
-		
+		this.root = null;
+		this.parentNodes[0].data = node;
+		this.parentNodes[0].priority = node;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
@@ -30,18 +36,56 @@ class MaxHeap {
 	}
 
 	clear() {
+		this.root = null;
+		this.parentNodes = [];
 		
 	}
 
 	insertNode(node) {
-		
+		if ( this.root === null) {
+			this.root = node;
+			this.parentNodes[0] = node;
+		} else {
+			var parent = this.parentNodes[0];
+			parent.appendChild(node);
+			this.parentNodes.push(node);
+			if (parent.right !== null) {
+				this.parentNodes.shift ();
+			}
+		}
 	}
+
+
+
+/**
+        10                       20
+       /  \                     /  \
+      5    7  - shift up ->   10   7
+     /                        /
+    20                       5
+**/
+
 
 	shiftNodeUp(node) {
-		
+	/*	if (this.root === node) {
+			return;
+		} else if ( node.priority < node.parent.priority) {
+			return;
+		} else {
+			node.swapWithParent();
+		}
+*/
+
+
+
+
+
+
 	}
 
+
 	shiftNodeDown(node) {
+
 		
 	}
 }
--- src/node.js
+++ src/node.js
@@ -1,23 +1,49 @@
 class Node {
 	constructor(data, priority) {
+		this.data = data;
+		this.priority = priority;	
 
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (this.left === null) {
+			this.left = node;
+			node.parent = this;
+		} else if (this.right === null) {
+			this.right = node;
+			node.parent = this;
+		} else {
+			return;
+		}
 	}
 
 	removeChild(node) {
-
+		if (this.left === node) {
+			this.left = null;
+		} else if (this.right === node) {
+			this.right = null;
+		} else {
+			throw ('err') ;
+		}
+		node.parent = null;
 	}
 
 	remove() {
+		if (this.parent === null) {
+			return;
+		} else {
+			this.parent.removeChild (this);
+		}
 
 	}
 
 	swapWithParent() {
-		
+	
 	}
 }
 
 module.exports = Node;
+                 
